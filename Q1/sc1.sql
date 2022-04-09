7c2e33ce0e9f8c0ad71c64cdd3c569790d27dbf6
mihirkumar1212 <59831687+mihirkumar1212@users.noreply.github.com>
2022-04-09 10:07:48 -0400

    Add files via upload

---
+++ Q1/sc1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS Covid19;
+USE Covid19;
+
+DROP TABLE IF EXISTS TimeSeries;
+
+
+CREATE TABLE IF NOT EXISTS TimeSeries (
+  id serial NOT NULL PRIMARY KEY,
+  Conformed_date DATE,
+  Country_Region varchar(200),
+  Province_State varchar(200),
+  Confirmed INT(255),
+  Recovered INT(255),
+  Deaths INT(255)
+);
+
+
